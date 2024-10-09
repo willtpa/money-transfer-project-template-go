657b9ba875f690085ad5315ee1949f7c2ff43d55
will <will@wills-MacBook-Pro.local>
2024-10-09 08:54:41 +0800

    chore(demo): no versioning

---
+++ activity.go
@@ -0,0 +1,61 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+)
+
+// @@@SNIPSTART money-transfer-project-template-go-activity-withdraw
+func Withdraw(ctx context.Context, data PaymentDetails) (string, error) {
+	// add delay 15sec
+	time.Sleep(15 * time.Second)
+
+	log.Printf("Withdrawing $%d from account %s.\n\n",
+		data.Amount,
+		data.SourceAccount,
+	)
+
+	referenceID := fmt.Sprintf("%s-withdrawal", data.ReferenceID)
+	bank := BankingService{"bank-api.example.com"}
+	confirmation, err := bank.Withdraw(data.SourceAccount, data.Amount, referenceID)
+	return confirmation, err
+}
+
+// @@@SNIPEND
+
+// @@@SNIPSTART money-transfer-project-template-go-activity-deposit
+func Deposit(ctx context.Context, data PaymentDetails) (string, error) {
+	// add delay 30sec
+	time.Sleep(30 * time.Second)
+
+	log.Printf("Depositing $%d into account %s.\n\n",
+		data.Amount,
+		data.TargetAccount,
+	)
+
+	referenceID := fmt.Sprintf("%s-deposit", data.ReferenceID)
+	bank := BankingService{"bank-api.example.com"}
+	// Uncomment the next line and comment the one after that to simulate an unknown failure
+	// confirmation, err := bank.DepositThatFails(data.TargetAccount, data.Amount, referenceID)
+	confirmation, err := bank.Deposit(data.TargetAccount, data.Amount, referenceID)
+	return confirmation, err
+}
+
+// @@@SNIPEND
+
+// @@@SNIPSTART money-transfer-project-template-go-activity-refund
+func Refund(ctx context.Context, data PaymentDetails) (string, error) {
+	log.Printf("Refunding $%v back into account %v.\n\n",
+		data.Amount,
+		data.SourceAccount,
+	)
+
+	referenceID := fmt.Sprintf("%s-refund", data.ReferenceID)
+	bank := BankingService{"bank-api.example.com"}
+	confirmation, err := bank.Deposit(data.SourceAccount, data.Amount, referenceID)
+	return confirmation, err
+}
+
+// @@@SNIPEND
